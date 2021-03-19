ce8fdebeed2067e4463e7463c6d9b268336eacca
fiveseven-lambda <fiveseven.lambda@gmail.com>
2021-03-19 16:53:19 +0900

    にゃーん！

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
-mod char;
+
 
 fn main() {
-    match char::read(&std::path::PathBuf::from("test")) {
-        Ok(_) => {}
-        Err(err) => {
-            println!("{}", err);
-        }
-    }
 }
